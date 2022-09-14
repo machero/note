7212b9b24bbb3d6133bddbff4a09d647a73ae2b7
xiaoshiran <xiaoshiran@lattebank.com>
2022-09-14 16:54:02 +0800

    add:AMQP learning note

---
+++ ray.py
@@ -0,0 +1,19 @@
+
+'''
+
+wether use the distribution computation ot distribution trainning
+tips：
+1、how to get the head node address
+2、how to set the work_number or wether use GPU
+
+
+aim：
+1、 input: start up the cluster、ray.init connect to the ray-cluster、
+
+'''
+
+
+
+
+class distribution():
+    ### 
